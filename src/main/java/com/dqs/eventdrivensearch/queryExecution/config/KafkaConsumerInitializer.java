9fbb7b07c7586adfa3fcdb9a6c8426ce1ca27659
ksn-crafter <kuldeep@caizin.com>
2025-05-28 16:41:24 +0530

    feat: make the consumer tenant configurable

--- src/main/java/com/dqs/eventdrivensearch/queryExecution/config/KafkaConsumerInitializer.java
+++ src/main/java/com/dqs/eventdrivensearch/queryExecution/config/KafkaConsumerInitializer.java
@@ -2,6 +2,7 @@ package com.dqs.eventdrivensearch.queryExecution.config;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,9 +13,12 @@ public class KafkaConsumerInitializer {
     @Autowired
     private DynamicKafkaConsumerConfiguration dynamicKafkaConsumerConfiguration;
 
+    @Value("${tenant_ids}")
+    private String tenants;
+
     @PostConstruct
     public void initializeListeners() {
-        List<String> tenantIds = List.of("jpmc", "deutsche");
+        List<String> tenantIds = List.of(tenants.split(","));
         for (String suffix : tenantIds) {
             dynamicKafkaConsumerConfiguration.registerConsumerForTopic(suffix);
         }
