d713f8b92c9848552cf00feb42bf62077e4bd25e
Kuldeep Singh  Nauhwar <kuldeep@caizin.com>
2025-08-08 08:54:37 +0530

    fix: address compiler warnings

--- src/main/java/com/dqs/eventdrivensearch/queryExecution/searchV2/S3Adapter.java
+++ src/main/java/com/dqs/eventdrivensearch/queryExecution/searchV2/S3Adapter.java
@@ -2,23 +2,26 @@ package com.dqs.eventdrivensearch.queryExecution.searchV2;
 
 import com.dqs.eventdrivensearch.queryExecution.search.metrics.MetricsPublisher;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.core.ResponseInputStream;
+
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
 
 import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.logging.Level;
 
 @Component
 public class S3Adapter {
-    private S3Client s3Client;
+    private final S3Client s3Client;
 
-    private MetricsPublisher metricsPublisher;
+    private final MetricsPublisher metricsPublisher;
 
     public S3Adapter(S3Client s3Client,MetricsPublisher metricsPublisher){
         this.s3Client = s3Client;
@@ -32,18 +35,17 @@ public class S3Adapter {
         Instant start = Instant.now();
 
         try {
-            URL url = new URL(s3IndexUrl);
+            URL url = new URI(s3IndexUrl).toURL();
             String bucketName = url.getHost().split("\\.")[0];
             String key = url.getPath().substring(1);
 
-            ResponseInputStream<?> responseInputStream = s3Client.getObject(GetObjectRequest.builder()
+            s3IndexInputStream = s3Client.getObject(GetObjectRequest.builder()
                     .bucket(bucketName)
                     .key(key)
                     .build());
-            s3IndexInputStream = responseInputStream;
 
-        } catch (MalformedURLException | NoSuchKeyException e) {
-            logger.log(Level.WARNING, e.getMessage() + "\n" + e.getStackTrace() + "\n" + "index url: " + s3IndexUrl);
+        } catch (MalformedURLException | NoSuchKeyException | URISyntaxException e) {
+            logger.log(Level.WARNING, e.getMessage() + "\n" + Arrays.toString(e.getStackTrace()) + "\n" + "index url: " + s3IndexUrl);
             throw new RuntimeException(e);
         }
 
