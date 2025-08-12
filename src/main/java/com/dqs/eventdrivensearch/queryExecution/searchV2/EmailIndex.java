b3f8d4e5fea7dad1d933fe04dd621237c16c8aba
Kuldeep Singh  Nauhwar <kuldeep@caizin.com>
2025-08-12 13:08:19 +0530

    chore: format file

--- src/main/java/com/dqs/eventdrivensearch/queryExecution/searchV2/EmailIndex.java
+++ src/main/java/com/dqs/eventdrivensearch/queryExecution/searchV2/EmailIndex.java
@@ -9,9 +9,9 @@ import org.apache.lucene.search.Query;
 public class EmailIndex {
 
     public Query getQuery(String queryString) throws ParseException {
-         final String[] EMAIL_FIELDS = {"body", "subject", "date", "from", "to", "cc", "bcc"};
+        final String[] EMAIL_FIELDS = {"body", "subject", "date", "from", "to", "cc", "bcc"};
 
-         final StandardAnalyzer analyzer = new StandardAnalyzer();
+        final StandardAnalyzer analyzer = new StandardAnalyzer();
 
         MultiFieldQueryParser parser = new MultiFieldQueryParser(EMAIL_FIELDS, analyzer);
         return parser.parse(queryString);
